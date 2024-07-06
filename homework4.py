0991776aaf024674561d0423b3d2e861bcdebf91
ArhiValeron <burdin.vv@gmail.com>
2024-07-06 19:02:08 +0500

    H
    G

---
+++ homework4.py
@@ -0,0 +1,9 @@
+int1 = 1111
+str1 = 'saddaa111dlads'
+immutable_var = (int1, 1, 'Stroke', str1)
+print(immutable_var)
+print(int1, str1)
+int1 = "dsadsadsad"
+str1 = 122333
+print(immutable_var)
+print(int1, str1)
