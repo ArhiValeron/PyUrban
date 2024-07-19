62ad36fdeb18c7fd2f577a01e6feb84a43a9f75c
ArhiValeron <burdin.vv@gmail.com>
2024-07-19 20:07:39 +0500

    1h

---
+++ module1hard.py
@@ -0,0 +1,39 @@
+# Задание "Средний балл":
+# Вам необходимо решить задачу из реальной жизни: "школьные учителя устали подсчитывать вручную средний балл каждого
+# ученика, поэтому вам предстоит автоматизировать этот процесс":
+#
+# На вход даны следующие данные:
+# Список: grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+# Множество: students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+#
+# Список grades содержит списки оценок для каждого ученика в алфавитном порядке.
+# Например: 'Aaron' - [5, 3, 3, 5, 4]
+# Множество students содержит неупорядоченную последовательность имён всех учеников в классе.
+#
+# Напишите программу, которая составляет словарь, используя grades и students, где ключом будет имя ученика, а значением - его средний балл.
+#
+# Вывод в консоль:
+# {'Aaron': 4.0, 'Bilbo': 2.25, 'Johhny': 4.0, 'Khendrik': 3.6666666666666665, 'Steve': 4.8}
+
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+journal: dict = {}
+j = 0
+grades_avarage = []
+for i in grades:
+    avarage_grade = 0
+    num_grades = 0
+    for ii in i:
+        num_grades += 1
+        avarage_grade += ii
+    grades_avarage.append(avarage_grade/num_grades)
+
+
+for alf_i in alphabet:
+    for stud_ii in students:
+        if alf_i in stud_ii[0]:
+            journal.update({stud_ii: grades_avarage.pop(0)})
+
+print(journal)
