87edc1ee171765a6e16938f4fdfa6b098d3cd086
ArhiValeron <burdin.vv@gmail.com>
2024-10-27 20:13:45 +0500

    tests_12_1

--- tests_12_2.py
+++ tests_12_2.py
@@ -2,11 +2,10 @@ import runner_and_tournament
 import unittest
 
 class TournamentTest(unittest.TestCase):
-    all_results = {}
 
     @classmethod
     def setUpClass(cls):
-        cls.all_results = {}
+        cls.all_results = []
 
     def setUp(self):
         self.speed1 = 10
@@ -16,30 +15,35 @@ class TournamentTest(unittest.TestCase):
         self.Andrey = runner_and_tournament.Runner("Andrey", self.speed2)
         self.Nick = runner_and_tournament.Runner("Nick", self.speed3)
 
+
+
     @classmethod
     def tearDownClass(cls):
-        for i, result in enumerate(cls.all_results.values()):
-            print(f"{i + 1}: {result}")
+        for element in cls.all_results:
+            print(element)
+
 
     def test_tournament_Usain_Nick(self):
         tournament = runner_and_tournament.Tournament(90, self.Usain, self.Nick)
-        self.all_results.update(tournament.start())
-        self.assertTrue(self.all_results[1] == "Usain")
-        self.assertTrue(self.all_results[2] == "Nick")
+        results = tournament.start()
+        self.assertTrue(results[1] == "Usain")
+        self.assertTrue(results[2] == "Nick")
+        self.all_results.append(results)
 
     def test_tournament_Andrey_Nick(self):
         tournament = runner_and_tournament.Tournament(90, self.Andrey, self.Nick)
-        self.all_results.update(tournament.start())
-        self.assertTrue(self.all_results[1] == "Andrey")
-        self.assertTrue(self.all_results[2] == "Nick")
+        results = tournament.start()
+        self.assertTrue(results[1] == "Andrey")
+        self.assertTrue(results[2] == "Nick")
+        self.all_results.append(results)
 
     def test_tournament_Usain_Andrey_Nick(self):
         tournament = runner_and_tournament.Tournament(90, self.Usain, self.Andrey, self.Nick)
-        self.all_results.update(tournament.start())
-        self.assertTrue(self.all_results[1] == "Usain")
-        self.assertTrue(self.all_results[2] == "Andrey")
-        self.assertTrue(self.all_results[3] == "Nick")
-
+        results = tournament.start()
+        self.assertTrue(results[1] == "Usain")
+        self.assertTrue(results[2] == "Andrey")
+        self.assertTrue(results[3] == "Nick")
+        self.all_results.append(results)
 
 if __name__ == '__main__':
 
